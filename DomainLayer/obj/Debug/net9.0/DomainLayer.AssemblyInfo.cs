c30e6db573e1df774ce21374ec36d3d54e30b32a
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-01-25 21:30:01 +0300

    Migration Changes

--- DomainLayer/obj/Debug/net9.0/DomainLayer.AssemblyInfo.cs
+++ DomainLayer/obj/Debug/net9.0/DomainLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478fba9a10a8f50db542a480236bca1f496fe7ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3f1c8a6940a461345af1629856a279dbd11659")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
