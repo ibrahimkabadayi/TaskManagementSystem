f2808948dfe8c381af8693ade44b807c84011772
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-01-29 11:54:44 +0300

    Password propertys length increased

--- PresentationLayer/Models/AuthenticateRequests/RegisterRequest.cs
+++ PresentationLayer/Models/AuthenticateRequests/RegisterRequest.cs
@@ -4,15 +4,7 @@ namespace TaskManagementSystem.Models.AuthenticateRequests;
 
 public class RegisterRequest
 {
-    [Required(ErrorMessage = "Name is required")]
-    [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters")]
-    public string Name { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Enter a valid email address")]
-    public string Email { get; set; } = string.Empty;
-
-    [Required(ErrorMessage = "Password is required")]
-    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters")]
-    public string Password { get; set; } = string.Empty;
+    public required string Name { get; set; } = string.Empty;
+    public required string Email { get; set; } = string.Empty;
+    public required string Password { get; set; } = string.Empty;
 }
